98423557ca6b59ca16b8e80d287363f7d7c8f45a
esraa <em5938941@gmail.com>
2025-08-28 22:04:14 +0300

    C44-G02-EF04

--- ConsoleApp15/Program.cs
+++ ConsoleApp15/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace ConsoleApp15
+﻿using ConsoleApp15.Data.Models;
+
+namespace ConsoleApp15
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using var context = new ITIDbContext();
+
+            var dept = new Department { Name = "CS", HiringDate = DateTime.Now };
+            context.Departments.Add(dept);
+            context.SaveChanges();
+
+            var departments = context.Departments.ToList();
+            foreach (var d in departments)
+                Console.WriteLine(d.Name);
+
+            var depToUpdate = context.Departments.First();
+            depToUpdate.Name = "Computer Science";
+            context.SaveChanges();
+
+            var depToDelete = context.Departments.First();
+            context.Departments.Remove(depToDelete);
+            context.SaveChanges();
         }
     }
 }
